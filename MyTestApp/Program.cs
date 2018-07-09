112541925b110c0a71eb25096f0c4abb9b42b3c1
DAVID HO <davidhorph@hotmail.com>
2018-07-09 12:47:33 -0500

    My changes...

---
+++ MyTestApp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace MyTestApp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("My Test App.");
+            Console.ReadLine();
+        }
+    }
+}
