89fcc473f4849863d18cf603f842a912dd33469f
DAVID HO <davidhorph@hotmail.com>
2018-07-09 12:50:09 -0500

    Another change. What will happen?

--- MyTestApp/Program.cs
+++ MyTestApp/Program.cs
@@ -7,6 +7,8 @@ namespace MyTestApp
         static void Main(string[] args)
         {
             Console.WriteLine("My Test App.");
+            Console.WriteLine("Here is another change. Will it save? Yes or no?");
+            string answer = Console.ReadLine();
             Console.ReadLine();
         }
     }
